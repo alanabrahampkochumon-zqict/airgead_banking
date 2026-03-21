21eca0a050b2e692d56c47dac8ef10b6043064d1
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-03-22 05:20:33 +0530

    chores: update documentation of utils

--- include/Config.h
+++ include/Config.h
@@ -5,7 +5,7 @@
  * @author Alan Abraham P Kochumon
  * @date Created on: March 29, 2026
  *
- * @brief Application configuration.
+ * @brief Configuration options for customizing look and Airgead UI.
  * 
  * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
  */
@@ -15,6 +15,6 @@ static constexpr const char* CONSOLE_COLOR = "\033[32m"; ///< Color configuratio
 static constexpr int COLUMN_WIDTH = 24; ///< Width of amount (money) column.
 static constexpr int COLUMNS = 3; ///< Number of columns in the table.
 
-static constexpr char CURRENCY_SYMBOL = '$'; ///< Currency used.
+static constexpr char CURRENCY_SYMBOL = '$'; ///< Currency symbol for formatting currencies.
 
 #endif
